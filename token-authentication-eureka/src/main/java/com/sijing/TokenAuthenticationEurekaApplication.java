3d45c105974e586e3f4db89c6de0890f656a995a
844485538 <844485538@qq.com>
2020-05-15 21:38:12 +0800

    first commit

---
+++ token-authentication-eureka/src/main/java/com/sijing/TokenAuthenticationEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.sijing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenAuthenticationEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenAuthenticationEurekaApplication.class, args);
+	}
+
+}
---
+++ token-authentication-service-one/src/main/java/com/sijing/TokenAuthenticationServiceOneApplication.java
@@ -0,0 +1,13 @@
+package com.sijing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenAuthenticationServiceOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenAuthenticationServiceOneApplication.class, args);
+	}
+
+}
